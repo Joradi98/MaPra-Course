023d59ed75274b297894d668610d7458195ecee3
Johannes Raufeisen <Joradi98@users.noreply.github.com>
2018-04-20 15:44:13 +0200

    Implemented Heap-Sort

--- Task2/Sortieren.cpp
+++ Task2/Sortieren.cpp
@@ -5,6 +5,55 @@
 using namespace std;
 
 
+/*
+Healper function for Heap-Sort:
+*/
+// To heapify a subtree rooted with node i which is
+// an index in arr[]. n is size of heap
+void heapify(unsigned int *&array, size_t length, int i)
+{
+	int largest = i;  // Initialize largest as root
+	int l = 2*i + 1;  // left = 2*i + 1
+	int r = 2*i + 2;  // right = 2*i + 2
+ 
+	// If left child is larger than root
+	if (l < length && array[l] > array[largest])
+		largest = l;
+ 
+	// If right child is larger than largest so far
+	if (r < length && array[r] > array[largest])
+		largest = r;
+ 
+	// If largest is not root
+	if (largest != i)
+	{
+		tausche(array, i, largest);
+
+		// Recursively heapify the affected sub-tree
+		heapify(array, length, largest);
+	}
+}
+ 
+
+
+/*
+Heap-Sort
+*/
+void heapSort(unsigned int *&array, int n)
+{
+	// Build heap
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(array, n, i);
+ 
+	// One by one extract an element from heap
+	for (int i=n-1; i>=0; i--) {
+		// Move current root to end
+		tausche(array, 0, i);
+ 
+		// call max heapify on the reduced heap
+		heapify(array, i, 0);
+	}
+}
 
 
 
@@ -217,6 +266,13 @@ int main(int argc, char *argv[]) {
 		cout << "Merge Sort: ";
 		printArray(array, length);
 
+		
+		start(2, length, array);
+		heapSort(array, length);
+		cout << "Heap Sort: ";
+		printArray(array, length);
+
+		
 		
 		delete[] array;
 	}
