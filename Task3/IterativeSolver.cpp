6a55d35c04ced02d1a8ed5fda939049e46139acd
Johannes Raufeisen <Joradi98@users.noreply.github.com>
2018-05-09 14:17:33 +0200

    Implemented Conjugate Gradients
    All tests pass.

--- Task3/IterativeSolver.cpp
+++ Task3/IterativeSolver.cpp
@@ -1,7 +1,7 @@
-/***********************************************
-*  Name       : iterativeSolve.cpp             *
-*  Verwendung : Implementation von Iterationsverfahren                   *
-***********************************************/
+/*******************************************************
+*  Name       : IterativeSolver.cpp                    *
+*  Verwendung : Implementation von Iterationsverfahren *
+********************************************************/
 
 #include "matrix.h"
 #include "vektor.h"
@@ -17,9 +17,7 @@ Vektor jacobi(Matrix A, Vektor x0, Vektor b, double tol, unsigned int maxIter, u
   Vektor oldVektor = x0;
   Vektor newVector = x0;
   
-  
-  //Eingabe ueberpruefen: Auf Diagonale darf keine Null stehen!
-  
+
   
   // Algorithmus durchfuehren bis gewuenschte Genuaigkeit oder max. Anzahl der Iterationen 
   for (i = 0; i < maxIter; i++) {
@@ -37,7 +35,6 @@ Vektor jacobi(Matrix A, Vektor x0, Vektor b, double tol, unsigned int maxIter, u
       
       //Komponente des neuen Vektors nach Formel berechnen
       newVector(k) = 1.0/A(k,k) * ( b(k) - sum );
-      
     }
     
     //Abbruchbedingung ueberpruefen.
@@ -46,18 +43,113 @@ Vektor jacobi(Matrix A, Vektor x0, Vektor b, double tol, unsigned int maxIter, u
       break;
     }
     
-    
     //Alten Vektor auf den neuen setzen, um nachste Iteration vorzubereiten
     oldVektor = newVector;
-
   }
   
   // Rueckgabe
   benoetigteIterationen = i;
   return newVector;
+}
+
+
+Vektor gauss_seidel(Matrix A, Vektor x0, Vektor b, double tol, unsigned int maxIter, unsigned int &benoetigteIterationen)
+{
+  unsigned int i, k, l;
+  double sum1, sum2;
+  Vektor oldVektor = x0;
+  Vektor newVector = x0;
   
+
+  
+  // Algorithmus durchfuehren bis gewuenschte Genuaigkeit oder max. Anzahl der Iterationen 
+  for (i = 0; i < maxIter; i++) {
+    //Iterationsschritt i
+    
+    for (k = 0; k < x0.Laenge(); k++) {
+      //Komponente k des neuen Vektors berechnen
+      
+      //Linke Summe aus der Vorschrift des GS-Verfahrens berechnen
+      sum1 = 0;
+      for (l = 0; l +1 <= k; l++) {
+        sum1 += A(k,l)*newVector(l);
+      }
+      
+      //Rechte Summe aus der Vorschrift des GS-Verfahrens berechnen
+      sum2 = 0;
+      for (l = k+1; l < x0.Laenge(); l++) {
+        sum2 += A(k,l)*oldVektor(l);
+      }
+      
+      //Komponente des neuen Vektors nach Formel berechnen
+      newVector(k) = 1.0/A(k,k) * ( b(k) - sum1 - sum2 );
+    }
+    
+    //Abbruchbedingung ueberpruefen.
+    if ( fabs( (b - A*newVector).Norm2()/b.Norm2()) < tol ) {
+      i++;
+      break;
+    }
+    
+    //Alten Vektor auf den neuen setzen, um nachste Iteration vorzubereiten
+    oldVektor = newVector;
+  }
+  
+  // Rueckgabe
+  benoetigteIterationen = i;
+  return newVector;
+}
+
+
+
+Vektor conjugate_gradients(Matrix A, Vektor x0, Vektor b, double tol, unsigned int maxIter, unsigned int &benoetigteIterationen)
+{
+  unsigned int i;
+  double alpha, gamma_old, gammma_new;
+  
+  Vektor x_old = x0;
+  Vektor x_new = x0;
+  
+  Vektor q(x0.Laenge()), p_old(x0.Laenge()), p_new(x0.Laenge()), r_old(x0.Laenge()), r_new(x0.Laenge());
+  
+  //Initialisierung
+  
+  p_old = b - A*x0;
+  r_old = p_old;
+  
+  gamma_old = dot(r_old, r_old);
+  
+  // Algorithmus durchfuehren bis gewuenschte Genuaigkeit oder max. Anzahl der Iterationen 
+  for (i = 0; i < maxIter; i++) {
+    //Iterationsschritt i
+
+    //Abbruchbedingung ueberpruefen.
+    if ( fabs(r_old.Norm2()/b.Norm2()) < tol ) {
+      break;
+    }
+
+    q = A * p_old;
+    alpha = gamma_old /dot(q,p_old) ;
+    x_new = x_old + alpha*p_old;
+    r_new = r_old - alpha*q;
+    gammma_new = dot(r_new, r_new);
+    p_new = r_new + (gammma_new/gamma_old) * p_old;
+        
+    //VNaechsten Schritt vorbereiten
+    x_old = x_new;
+    gamma_old = gammma_new;
+    p_old = p_new;
+    r_old = r_new;
+  }
+  
+  // Rueckgabe
+  benoetigteIterationen = i;
+  return x_new;
   
 }
+
+
+
 
 int main(int argc, char* argv[]) 
 {
@@ -71,18 +163,28 @@ int main(int argc, char* argv[])
 
   double tol = 0;
   
-  for (i=1; i <= AnzahlBeispiele; i++) {
+  for (i=1; i <= AnzahlBeispiele; i++) {  
     Start(i, A, x0, b, tol, maxIter);
-
     Vektor result(x0.Laenge());
+
     result = jacobi(A, x0, b, tol, maxIter, anzahlIter);
-    Ergebnis(result, anzahlIter, 0);
+    Ergebnis(result, anzahlIter, 0); // Jacobi entspricht 0
+    
+    Start(i, A, x0, b, tol, maxIter);
+    result = gauss_seidel(A, x0, b, tol, maxIter, anzahlIter);
+    Ergebnis(result, anzahlIter, 1); // GaussSeidel entspricht 1 
+
+    Start(i, A, x0, b, tol, maxIter);
+    result = conjugate_gradients(A, x0, b, tol, maxIter, anzahlIter);
+    Ergebnis(result, anzahlIter, 2); // CG entspricht 2
+
+    
     
     //TODO: Nummer des Beispiels als Kommandozeilenparameter uebergeben
     //TODO: LGS loesen
     //TODO: Relatives Residuum in Datei schreiben, gnuplot?
     //TODO: Ergebnis aufrufen
-
+    // Vielleicht TODO: Matrix ueberpruefen, sodass keine Nullen auf Diagonale stehen
   }
   
 
